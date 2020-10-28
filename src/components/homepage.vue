ac7dcc18b2f2fdde5db350dd78b5181c4a67489a
Peter Verhaar <peter_verhaar@live.nl>
2020-10-28 20:02:52 +0100

    init commit

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
+  <div class="container-fluid">
     <h1>Homepage</h1>
       <div class="row">
-        <div class="col-6">
+        <div class="col-12 col-md-12 col-lg-6 col-xl-6">
           <p>STEAM:</p>
-          <img src="../images/STEAMQR.png" alt="STEAMQR">
+          <img width="400" height="400" src="../images/STEAMQR.png" alt="STEAMQR">
         </div>
-        <div class="col-6">
+        <div class="col-12 col-md-12 col-lg-6 col-xl-6">
           <p>STEAM DB:</p>
-          <img  src="../images/STEAMDBQR.png" alt="STEAMQR">
+          <img width="400" height="400" src="../images/STEAMDBQR.png" alt="STEAMQR">
         </div>
       </div>
   </div>
