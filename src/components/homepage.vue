5803bdc8ac4a4d29ff9bcdbf95cc54c4b738cb7a
Peter Verhaar <peter_verhaar@live.nl>
2020-10-29 09:39:29 +0100

    QR codes set

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -1,27 +1,66 @@
 <template>
   <div class="container-fluid">
-    <h1>Homepage</h1>
-      <div class="row">
-        <div class="col-12 col-md-12 col-lg-6 col-xl-6">
-          <p>STEAM:</p>
-          <img width="400" height="400" src="../images/STEAMQR.png" alt="STEAMQR">
-        </div>
-        <div class="col-12 col-md-12 col-lg-6 col-xl-6">
-          <p>STEAM DB:</p>
-          <img width="400" height="400" src="../images/STEAMDBQR.png" alt="STEAMQR">
+    <div class="row">
+      <div class="mt-3 col-12 col-md-12 col-lg-6 col-xl-6">
+        <p>Favourite Anime</p>
+        <div class="row">
+          <div class="col-6"><img src="" alt="bleach"></div><div class="col-6">Bleach</div>
+          <div class="col-6"><img src="" alt="Sidonia no kishi"></div><div class="col-6">Bleach</div>
+          <div class="col-6"><img src="" alt="That Time I Got Reincarnated as a Slime"></div><div class="col-6">That Time I Got Reincarnated as a Slime</div>
         </div>
       </div>
+      <div class="mt-3 col-12 col-md-12 col-lg-6 col-xl-6">
+        <p>-</p>
+      </div>
+    </div>
+    <!-- QR codes -->
+    <div class="row">
+      <div class="mt-3 col-12 col-md-12 col-lg-6 col-xl-6">
+        <p>STEAM:</p>
+        <img
+          width="400"
+          height="400"
+          src="../images/STEAMQR.png"
+          alt="STEAM QR"
+        />
+      </div>
+      <div class="mt-3 col-12 col-md-12 col-lg-6 col-xl-6">
+        <p>STEAM DB:</p>
+        <img
+          width="400"
+          height="400"
+          src="../images/STEAMDBQR.png"
+          alt="STEAM DB"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="mt-3 col-12 col-md-12 col-lg-6 col-xl-6">
+        <p>Deviantart:</p>
+        <img
+          width="400"
+          height="400"
+          src="../images/DeviantartQR.png"
+          alt="Deviantart QR"
+        />
+      </div>
+      <div class="mt-3 col-12 col-md-12 col-lg-6 col-xl-6">
+        <p>Youtube:</p>
+        <img
+          width="400"
+          height="400"
+          src="../images/YTQR.png"
+          alt="Youtube QR"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'homepage',
-    methods:{
-
-    },
-    mounted(){
-
-    }
-}
+  name: "homepage",
+  methods: {},
+  mounted() {},
+};
 </script>
